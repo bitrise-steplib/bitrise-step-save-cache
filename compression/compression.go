7dc682800ae52bdf965c83d9e1396f181a7635b6
Olivér Falvai <oliver.falvai@bitrise.io>
2022-08-05 11:22:33 +0200

    Skip compression when paths are all empty

--- compression/compression.go
+++ compression/compression.go
@@ -1,6 +1,11 @@
 package compression
 
 import (
+	"errors"
+	"io"
+	"io/fs"
+	"os"
+
 	"github.com/bitrise-io/go-utils/v2/command"
 	"github.com/bitrise-io/go-utils/v2/env"
 	"github.com/bitrise-io/go-utils/v2/log"
@@ -31,4 +36,41 @@ func Compress(archivePath string, includePaths []string, logger log.Logger, envR
 	}
 
 	return nil
+}
+
+func AreAllPathsEmpty(includePaths []string) bool {
+	allEmpty := true
+
+	for _, path := range includePaths {
+		// Check if file exists at path
+		fileInfo, err := os.Stat(path)
+		if errors.Is(err, fs.ErrNotExist) {
+			// File doesn't exist
+			continue
+		}
+
+		// Check if it's a directory
+		if !fileInfo.IsDir() {
+			// Is a file and it exists
+			allEmpty = false
+			break
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			continue
+		}
+		_, err = file.Readdirnames(1) // query only 1 child
+		if errors.Is(err, io.EOF) {
+			// Dir is empty
+			continue
+		}
+		if err == nil {
+			// Dir has files or dirs
+			allEmpty = false
+			break
+		}
+	}
+
+	return allEmpty
 }
