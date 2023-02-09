f122ec1ec2d6de1262f49e1c6b03db45dcb1c1f3
Olivér Falvai <oliver.falvai@bitrise.io>
2023-02-09 15:51:03 +0100

    Remove beta label and other cleanups (#25)
    * Remove beta label and other cleanups
    
    * Use ExitCode constants
    
    * Remove dot import

--- main.go
+++ main.go
@@ -6,16 +6,18 @@ import (
 	"github.com/bitrise-io/go-steputils/v2/stepconf"
 	"github.com/bitrise-io/go-utils/v2/command"
 	"github.com/bitrise-io/go-utils/v2/env"
+	"github.com/bitrise-io/go-utils/v2/exitcode"
 	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/bitrise-io/go-utils/v2/pathutil"
 	"github.com/bitrise-steplib/bitrise-step-save-cache/step"
 )
 
 func main() {
-	os.Exit(run())
+	exitCode := run()
+	os.Exit(int(exitCode))
 }
 
-func run() int {
+func run() exitcode.ExitCode {
 	logger := log.NewLogger()
 	envRepo := env.NewRepository()
 	inputParser := stepconf.NewInputParser(envRepo)
@@ -25,13 +27,10 @@ func run() int {
 	pathModifier := pathutil.NewPathModifier()
 	cacheStep := step.New(logger, inputParser, cmdFactory, pathChecker, pathProvider, pathModifier, envRepo)
 
-	exitCode := 0
-
 	if err := cacheStep.Run(); err != nil {
 		logger.Errorf(err.Error())
-		exitCode = 1
-		return exitCode
+		return exitcode.Failure
 	}
 
-	return exitCode
+	return exitcode.Success
 }
